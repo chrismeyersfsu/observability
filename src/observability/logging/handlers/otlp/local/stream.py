2ea017f89cfd2fad524a94f45eeb69c24a2166ff
Chris Meyers <chrismeyersfsu@users.noreply.github.com>
2024-12-22 08:39:24 -0500

    remove uneeded import

--- src/observability/logging/handlers/otlp/local/stream.py
+++ src/observability/logging/handlers/otlp/local/stream.py
@@ -4,8 +4,6 @@ import logging
 
 from observability.logging.handlers.adapters.otlp import BaseJsonHandler
 
-from ansible_base.lib.observability.logging.filters.otlp import JSONNLFormatter
-
 
 class OTLPStreamHandler(BaseJsonHandler):
     def __init__(self):
