bcfba40b831791a46214a2c69aa41d2b849c57b9
Chris Meyers <chrismeyersfsu@users.noreply.github.com>
2024-12-22 08:45:30 -0500

    move json record processor

--- src/observability/logging/handlers/adapters/otlp.py
+++ src/observability/logging/handlers/adapters/otlp.py
@@ -1,51 +1,13 @@
 """Translate OTLP specific data-structures to python log handler."""
 
-import logging
-import typing
-
-from opentelemetry.sdk._logs.export import (
-    LogExporter,
-    LogExportResult,
-)
-from opentelemetry.sdk._logs import LogData
+from observability.logging.handlers.otlp.base import BaseOTLPHandler
 from opentelemetry.sdk._logs.export import (
     SimpleLogRecordProcessor,
 )
-from opentelemetry.proto.logs.v1.logs_pb2 import (
-    ResourceLogs,
-)
-from opentelemetry.exporter.otlp.proto.common._log_encoder import encode_logs
 from opentelemetry._logs import get_logger_provider
-
-from observability.logging.handlers.otlp.base import BaseOTLPHandler
 from observability.logging.filters.otlp import JSONNLFormatter
 
-
-class JsonExporterHandlerAdapter(LogExporter):
-    """Bridge OTLP exporter and python handler.
-
-    python handler
-      opentelemetry processor
-        opentelemetry exporter.export()
-          python handler.emit()
-    """
-
-    def __init__(self, handler):
-        self._handler = handler
-        super().__init__()
-
-    def _translate_data(
-        self,
-        data: typing.Sequence[LogData]
-    ) -> typing.List[ResourceLogs]:
-        return encode_logs(data)
-
-    def export(self, batch: typing.Sequence[LogData]) -> LogExportResult:
-        self._handler.emit(self._translate_data(batch))  # this is the magic
-        return LogExportResult.SUCCESS
-
-    def shutdown(self) -> None:
-        pass
+from observability.common.record_processor import JsonExporterHandlerAdapter
 
 
 class BaseJsonHandler(BaseOTLPHandler):
